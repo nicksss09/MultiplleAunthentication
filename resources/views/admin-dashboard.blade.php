387fe7e8ef2e494dc5438bfbbe57dfc16758a9e2
Rafael <rafaelalpes39@gmail.com>
2022-07-15 14:27:51 +0800

    alpes

--- resources/views/admin-dashboard.blade.php
+++ resources/views/admin-dashboard.blade.php
@@ -3,7 +3,7 @@
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Dashboard') }}
 <h1>  this is the interface for admin</h1>
-
+<h1>this line is added by me " alpes"</h1>
         </h2>
     </x-slot>
 </x-app-layout>
